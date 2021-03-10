167080561aeecde56322709e8e99a5ba9a287c8e
Lucas César <lucasambrogi1@hotmail.com>
2021-03-09 23:30:48 -0300

    typeorm configuration.

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
+import './database'
+
 import express from "express";
+import routes from './routes';
 
 const app = express();
 
-const number = 0.1;
-
-const number2 = 0.1;
-
-console.log(0.1 + 0.2 == 0.3);
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
     console.log('Server listening on port 3333');
