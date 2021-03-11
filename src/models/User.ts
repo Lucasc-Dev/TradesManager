7e7897c3f4e08e667ae0b044ed21621aa3eb04a4
Lucas César <lucasambrogi1@hotmail.com>
2021-03-10 23:58:54 -0300

    User entity and its repository: created.

---
+++ src/models/User.ts
@@ -0,0 +1,34 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import Trade from "./Trade";
+
+@Entity('users')
+export default class User {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @OneToMany(
+        type => Trade, 
+        trade => trade.user, 
+        { eager: true, cascade: ['insert', 'update'] }
+    )
+    @JoinColumn({ name: 'user_id' })
+    trades: Trade[];
+
+    @Column()
+    username: string;
+
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    credits: number;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
